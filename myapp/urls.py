1019b28206b510295e8a4d71bdd55c6ec6fbed1d
nabilbinadam <nabilbinadam@gmail.com>
2024-12-08 06:17:01 +0800

    adding few pages

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,10 +3,19 @@
 from django.urls import path
 from .views import item_list
 from .views import form_input
+from .views import loginPage
+from .views import signUp
+from .views import dashboard    
+from .views import create_task_view, task_list_view
 
 
 urlpatterns = [
     path('', item_list, name='item_list'),
     path('form/', form_input, name='form'),
+    path('login/',loginPage, name='login'),
+    path('signup/',signUp, name='signUp'),
+    path('dashboard/',dashboard ,name='dashboard'),
+    path('create_task_view/',create_task_view,name='create_task_view'),
+    path('task_list_view/',task_list_view,name='task_list_view'),
 ]
 
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render,redirect
 from .models import Item,Form
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+
+
+
+
 def item_list(request):
     return render(request,"item_list.html")
 
@@ -19,3 +26,64 @@ def form_input(request):
     return render(request, "form.html")
 
 
+
+def loginPage(request):
+
+    #get input in html
+    #check wether is in database
+    #redirect
+
+    if request.method == "POST":
+        username=request.POST.get("username")
+        password=request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+
+
+        if user not in None:
+            login(request,user)
+            return redirect('/dashboard')
+        else:
+            return render(request, 'loginpage.html', {'error': 'Invalid credentials'})
+
+
+    return render(request,"login.html")
+
+
+def signUp(request):
+    if request.method == "POST":
+        user= request.POST.get("username")
+        email=request.POST.get("email")
+        passw= request.POST.get("password")
+        confirm_password = request.POST.get('confirm_password')
+
+
+        if passw == confirm_password:
+            
+            try:
+                user = object.create.user(user="username",email="email",passw="password",confirm_password="confirm_password")
+                messages.succes(request,"Succes")
+                return redirect(request,'login.html')
+                
+            except Exception as e:
+                messages.error(request,str(e))
+        else:
+                messages.error(request,'passwor error')
+
+
+    return render(request,"signUp.html")
+
+
+
+def dashboard(request):
+
+
+    return render(request,"dashboard.html")
+
+
+
+
+def create_task_view(request):
+    return render(request, 'create_task.html')  
+
+def task_list_view(request):
+    return render(request, 'task_list_view.html') 
